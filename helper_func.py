4dbcb66e8e27106d41279c20ff40ecfae9563031
shinubo28always <shinubo28always@gmail.com>
2025-04-06 08:42:42 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,7 +10,6 @@ from pyrogram.errors import FloodWait
 from shortzy import Shortzy
 from database.database import user_data, db_verify_status, db_update_verify_status
 
-
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
@@ -27,7 +26,7 @@ async def is_subscribed(filter, client, update):
     else:
         return True
 
-async def is_subscribed2(filter, client, update):
+async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL2:
         return True
     user_id = update.from_user.id
@@ -37,11 +36,6 @@ async def is_subscribed2(filter, client, update):
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
     except UserNotParticipant:
         return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
 
 async def encode(string):
     string_bytes = string.encode("ascii")
